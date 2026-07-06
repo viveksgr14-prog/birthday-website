7d19d85043a6b15780eafcef83a9fba1519863cc
viveksgr14-prog <viveksgr14@gmail.com>
2026-07-07 00:21:41 +0530

    Updated birthday website

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const cake = document.querySelector(".cake");
 const container = document.querySelector(".container");
 
 const birthdayMessage = `
-🎂 Happy Birthday, ANKITHA! ❤️
+🎂 Happy Birthday, NAME! ❤️
 
 From the day we became friends,
 life became brighter.
